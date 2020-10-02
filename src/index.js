ac93edc1fd889d279e082496f9f491f4f2a62ee1
vovantais <vladimir0297@mail.ru>
2020-10-02 19:52:53 +0300

    init : start task and finish

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array = 0) {
+	if (array == 0 || array == '') {
+		return 0;
+	}
+	let res = array[0];
+	for (let i = 0; i < array.length; i++) {
+		if (res > array[i]) {
+			res = array[i];
+		}
+	}
+	return res;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array = 0) {
+	if (array == 0 || array == '') {
+		return 0;
+	}
+	let res = array[0];
+	for (let i = 0; i < array.length; i++) {
+		if (res < array[i]) {
+			res = array[i];
+		}
+	}
+	return res;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array = 0) {
+	if (array == 0 || array == '') {
+		return 0;
+	}
+	let res = 0;
+	array.forEach((item,) => {
+		res += item;
+	})
+	return res / array.length;
 }
